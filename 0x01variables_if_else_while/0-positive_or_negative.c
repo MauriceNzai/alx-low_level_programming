73dfdd111ee452701988f4b3441d00ec54d407fc
Maurice Haro <maurice.nzai@gmail.com>
2022-07-01 03:43:55 -0700

    Is number n positive, negative or zero?

---
+++ 0x01variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+  
+{
+  
+  int n;
+  
+
+  
+  srand(time(0));
+  
+  n = rand() - RAND_MAX / 2;
+  
+/**
+   *Tells if rndom generated number is positive, negative or zero
+*/
+  if (n > 0)
+    {
+      Printf(" is positive\n");
+    }
+  else if (x < 0)
+    {
+      printf(" is negative\n");
+    }
+  else
+    {
+      printf4(" is zero\n");
+    }
+  return (0);
+  
+}
