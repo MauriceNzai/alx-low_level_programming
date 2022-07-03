39cc0e04005a990c282519397dad2a912006afeb
Maurice Haro <maurice.nzai@gmail.com>
2022-07-03 06:28:06 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+/**
+ *This program prints all single digit numbers off base 10
+ *starting from 0 and followed by a new line
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  int i;
+  for (i = 0; i <10; i++)
+    {
+      printf("%d", i);
+
+      printf("\n");
+    }
+
+  return (0);
+}
