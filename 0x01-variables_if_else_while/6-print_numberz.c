6b3951d5e3a5421fda5d15e5ac5ffdc582621641
Maurice Haro <maurice.nzai@gmail.com>
2022-07-03 06:41:27 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+/**
+ *This program prints all single digit numbers of base 10
+ *starting from 0 and followed by a new line
+ *
+ *Using the putchar function
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int num;
+
+  for (num = '0'; num <= '9'; num++)
+    {
+      putchar(num);
+      putchar('\n');
+    }
+  
+  return (0);
+
+}
