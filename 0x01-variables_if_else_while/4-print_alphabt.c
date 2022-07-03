b6dbd2efb3a15e9abc516a08163ccd6d1f39dbfb
Maurice Haro <maurice.nzai@gmail.com>
2022-07-03 06:14:04 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,37 @@
+/**
+ *This program prints the letters of the aphabet
+ *in lowercase except letters q and e
+ *Only putchar function is allowed
+ */
+
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char lower;
+  char e;
+  char q;
+
+  e = 'e';
+  q= 'q';
+
+  for (lower ='a'; lower <= 'z'; lower++)
+    {
+      if (lower != e && lower != q
+	{
+	  putchar(lower);
+	}
+    }
+
+      putchar('\n');
+
+      return (0);
+    }
+      
+
