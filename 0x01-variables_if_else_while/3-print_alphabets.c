64d8682ebdb606777b61ca8b7ac4426cfc752478
Maurice Haro <maurice.nzai@gmail.com>
2022-07-03 04:59:48 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+/**
+*his program will print the alphabet
+*in lowercase and uppercase
+*followed by a neline
+*using the putchar function
+*/
+
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* return: Always 0 (Success)
+*/
+
+int maint(void)
+{
+  char lower;
+
+  for (lower = 'a'; lower <= 'z'; lower++)
+    {
+      putchar(lower);
+    }
+  for (lower = 'A'; lower <= 'Z'; lower++)
+    {
+      putchar(lower);
+    }
+  putchar('\n');
+
+  return (0);
+}
