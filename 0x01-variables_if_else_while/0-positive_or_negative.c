7a43ed3516a4af03c3c8757122a9460157393f1e
Maurice Haro <maurice.nzai@gmail.com>
2022-07-03 00:42:51 -0700

    Number Positive of Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,27 +12,27 @@
 
 int main(void)  
 {
-  int n;
+int n;
    
-  srand(time(0));
+srand(time(0));
   
-  n = rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
   
 /**
-   *Tells if rndom generated number is positive, negative or zero
+*Tells if rndom generated number is positive, negative or zero
 */
-  if (n > 0)
-    {
-      printf("%d is positive\n", n);
-    }
-  else if (n < 0)
-    {
-      printf("%d is negative\n", n);
-    }
-  else
-    {
-      printf("%d is zero\n", n);
-    }
-  return (0);
+if (n > 0)
+{
+  printf("%d is positive\n", n);
+ }
+else if (n < 0)
+{
+  printf("%d is negative\n", n);
+}
+ else
+{
+  printf("%d is zero\n", n);
+}
+return (0);
   
 }
