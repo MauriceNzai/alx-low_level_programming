174dbc72f69ec7c4512b9bb6ac9fb09efe773f56
Maurice Haro <maurice.nzai@gmail.com>
2022-07-04 04:19:23 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -27,14 +27,14 @@ n = rand() - RAND_MAX / 2;
  *Determine if numeber is Positive, Zero or Negative
  */
 
-if (n > 0)
-  printf("%d is positive\n", n);
+ if (n > 0)
+ printf("%d is positive\n", n);
 
-else  if (n == 0)
-  printf("%d is zero\n", n);
+ else  if (n == 0)
+ printf("%d is zero\n", n);
 
-else
-printf("%d is negative\n", n);
+ else
+ printf("%d is negative\n", n);
 
 return (0);
 }
