76ee6b16aab6d5e5bd72de810f03eb6515575b1d
Maurice Haro <maurice.nzai@gmail.com>
2022-07-04 04:49:39 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -18,14 +18,14 @@ int main(void)
 char lower;
 
 for (lower = 'a'; lower <= 'z'; lower++)
-  {
-  putchar(lower);
-  }
+{
+putchar(lower);
+}
 
 for (lower = 'A'; lower <= 'Z'; lower++)
-  {
-  putchar(lower);
-  }
+{
+putchar(lower);
+}
 
 putchar('\n');
 
