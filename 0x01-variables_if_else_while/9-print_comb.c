86984e0e5a3e49b27a4429fdbb4ea9c89ac3cfc0
Maurice Haro <maurice.nzai@gmail.com>
2022-07-04 05:54:59 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -18,18 +18,18 @@
 
 int main(void)
 {
-  int x;
+int x;
 
-  for (x = '0'; x <= '9'; x++)
-    {
-      putchar(x);
-      if (x != '9')
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
+for (x = '0'; x <= '9'; x++)
+{
+putchar(x);
+if (x != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
 
-  return (0);
+return (0);
 }
