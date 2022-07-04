0e151a12d66c3404927c0a4af450eb3ea33c2863
Maurice Haro <maurice.nzai@gmail.com>
2022-07-04 11:19:26 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point and the main block
+ * Description: Print all possible combinations of two digits
+ * The two digits must be differnt 
+ * 01 and 10 are considered the same combination of 0 and 1
+ * print only the smallest combination of the two digits
+ * Numbers should be printed in ascending order, with two digits
+ * You can only use the putchar function and a maximun 5 times
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+  int x;
+  int y;
+
+  for (x = '0'; x < '9'; x++)
+  {
+    for (y = x + 1; y <= '9'; y++)
+    {
+      if (x != y)
+      {
+	putchar(x);
+	putchar(y);
+
+	if (x == '8' && y == '9')
+	continue;
+
+
+	putchar(',');
+	putchar(' ');
+
+      }
+    }
+  }
+  putchar('\n');
+  return(0);
+}
