865f03af0ad3f91af12d65d8be7de1aa40ffbd6f
Maurice Haro <maurice.nzai@gmail.com>
2022-07-04 11:52:51 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point and  the main block
+ *
+ * Print all possible combinations of three digits
+ * Numbers must be separeated by a comma followed by a space
+ * The three digits must be different
+ * 012, 120,102,021,201,210 are considered same combinations of 0, 1 and 2
+ * You can only use the putchar function and only a maximun 6 times
+ * You are not allowed to use any variable of type char
+ * All your code to be in the main function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
+int x;
+int y;
+int z;
+
+for (x = '0'; x < '9'; x++)
+{
+for (y = x+1; y <= '9'; y++)
+{
+for (z = y+1: z <= '9'; Z++)
+{
+if ((z != x) != y)
+{
+putchar(x);
+putchar(y);
+putchar(z);
+
+if (x == '7' && y == '8')
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
