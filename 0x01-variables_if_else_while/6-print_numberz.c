652fc5785c3a9dec37a99aa78d85fb0bddb1dafb
Maurice Haro <maurice.nzai@gmail.com>
2022-07-04 05:29:00 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -14,14 +14,14 @@
  */
 int main(void)
 {
-  int num;
+int num;
 
-  for (num = '0'; num <= '9'; num++)
-    {
-      putchar(num);
-      putchar('\n');
-    }
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+putchar('\n');
+}
   
-  return (0);
+return (0);
 
 }
