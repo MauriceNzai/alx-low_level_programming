12c6bafae6eaf78b23cfe98ec0ab24a39c6b7ce7
Maurice Haro <maurice.nzai@gmail.com>
2022-07-04 02:34:28 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -18,15 +18,16 @@ int main(void)
   int n;
   char lower;
 
-  for (n ='0'; n <= '9'; n++);
+  for (n = '0'; n <= '9'; n++);
   {  
     putchar(n);
+  }
   
   for (lower = 'a'; lower <= 'f'; lower++)
     { 
       putchar(lower);
     }   
     putchar('\n');
-  }
+  
 return (0);
 }
