150bb6f1091933381b062d7ecc0f26718112ebf9
Maurice Haro <maurice.nzai@gmail.com>
2022-07-05 00:40:18 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,13 +7,15 @@
 /**
  * main - Entry point and main block
  *
- * Description: This program will determine whether a randomly generated number
+ * This program will determine whether a randomly generated number
+ * 
  * The output of the program should be,
+ * 
  * The number, followed by is positive, if the number is Positive
  * The number, followed by is zero, if the number is Zero
  * The number, followed by is negative,if the number is Negative
  *
- * return: 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
@@ -23,10 +25,6 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-
-/**
- *Determine if numeber is Positive, Zero or Negative
- */
 
 if (n > 0)
 printf("%d is positive\n", n);
