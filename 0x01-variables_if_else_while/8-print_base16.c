d519b805f8b751dac66b7a1551597742ca1fa600
Maurice Haro <maurice.nzai@gmail.com>
2022-07-05 01:20:17 -0700

    Tsak 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -17,7 +17,7 @@ int n;
 char lower;
 
 for (n = '0'; n <= '9'; n++)
-
+{
 putchar(n);
 
 for (lower = 'a'; lower <= 'f'; lower++)
@@ -25,6 +25,6 @@ for (lower = 'a'; lower <= 'f'; lower++)
 putchar(lower);
 putchar('\n');
 }
-
+}
 return (0);
 }
