aefe0500c648db525e2399960c87ee30ecd51ed8
Maurice Haro <maurice.nzai@gmail.com>
2022-07-06 00:20:53 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_putchar - Prints _putchar
+ *
+ * Description: This fuction prints _putchar followed by a new line
+ *
+ * Return: 0 Always (Success)
+ */
+
+void print_putchar(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+}
