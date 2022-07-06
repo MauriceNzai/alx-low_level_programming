c722958e3af5c044a1bd290a022156013eee2152
Maurice Haro <maurice.nzai@gmail.com>
2022-07-06 12:00:22 -0700

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,71 @@
+#include "main.c"
+
+/**
+ * print_times_table - This function prints the times table starting with 0
+ * If n is greater the 15 or less than 0, the function prints nothing
+ *
+ * @n: An integer value determing the times table to print
+ *
+ * Return: Nothing
+ */
+
+void print_times_table(int n)
+{
+
+int x;
+int y;
+
+if (n > 0 && n < 15)
+{
+
+for (x = 0; x <= n; x++)
+{
+_putchar('0');
+
+for (y = 1; y <= n; y++)
+
+putformat(x * y);
+_putchar('\n');
+}
+}
+}
+
+/**
+ * putformat - This function formats the output
+ *
+ * @n: The number to format
+ *
+ * Return: Nothing
+ */
+
+void putformat(int n)
+{
+
+if (n <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(n + '0');
+}
+
+else if (n > 9 && n <= 99)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(n / 10 + '0');
+_putchar(n % 10 + '0');
+}
+
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(n / 100 + '0');
+_putchar(n / 10 % 10 + '0');
+_putchar(n % 10 + '0');
+}
+
+}
