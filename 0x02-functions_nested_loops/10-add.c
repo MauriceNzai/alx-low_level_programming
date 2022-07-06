1ae0fe8a274ec0445c9a949cb87d2413392ebaf9
Maurice Haro <maurice.nzai@gmail.com>
2022-07-06 07:07:33 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - This function add two integers and returns the result.
+ *
+ * @x: An integer input
+ * @y: An integer input
+ *
+ * Return: result of addition
+ */
+
+int add(int x, int y)
+{
+return (x + y);
+}
