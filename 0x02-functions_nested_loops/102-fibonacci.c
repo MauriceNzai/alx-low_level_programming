fc1a5b40135a12ed8204ae8d6d6e9485a4edeea8
Maurice Haro <maurice.nzai@gmail.com>
2022-07-07 03:38:24 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -18,7 +18,7 @@ long int fibonacci[50];
 fibonacci[0] = 1;
 fibonacci[1] = 2;
 
-printf("%ld, %ld", fibonacci[0], fibonacci[1]);
+printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
 for (x = 2; x < 50; x++)
 {
