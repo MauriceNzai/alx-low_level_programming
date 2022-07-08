47edfb799d0ace6ca867bb686366a3e60f4fa8ee
Maurice Haro <maurice.nzai@gmail.com>
2022-07-08 02:40:47 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - This function prints the numbers from 0 to 14
+ * 10 times followed by a new line
+ * You can only use the _putchar function and only 3 times
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+int x;
+int y;
+
+ for (x = 0; x <= 9; x++)
+{
+
+for (y = 0; y <= 14; y++)
+{
+
+_putchar((y / 10) + '0');
+_putchar((y % 10) + '0');
+}
+_putchar('\n');
+}
+}
