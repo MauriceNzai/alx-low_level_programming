97294f9db0c4cf63525d425410dbcc439a45a8b7
Maurice Haro <maurice.nzai@gmail.com>
2022-07-08 01:00:52 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - This function print numbers
+ * from 0 to 9, followed by a new line
+ * You can only use _putchar function, and only twice
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+int x;
+
+for (x = '0'; x <= 9; x++)
+{
+_putchar(x);
+_putchar('\n');
+}
+
+}
