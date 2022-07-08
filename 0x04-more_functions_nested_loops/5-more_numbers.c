5cc616dcd55e0b4165d3151c59cce06a60a06ca0
Maurice Haro <maurice.nzai@gmail.com>
2022-07-08 02:46:20 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,12 +13,12 @@ void more_numbers(void)
 int x;
 int y;
 
- for (x = 0; x <= 9; x++)
+for (x = 0; x <= 9; x++)
 {
 
 for (y = 0; y <= 14; y++)
 {
-
+if (y > 9)
 _putchar((y / 10) + '0');
 _putchar((y % 10) + '0');
 }
