bdd62711ba97fde28555482217900b239db2ec38
Maurice Haro <maurice.nzai@gmail.com>
2022-07-12 00:23:53 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - This function takes a pointer to an integer as a parameter
+ * and resets the value it points to, to 98
+ *
+ * @*n: Pointer to an integer value
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
