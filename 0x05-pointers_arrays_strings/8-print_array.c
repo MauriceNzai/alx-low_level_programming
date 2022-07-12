481e5bc43629253567eea9ef7baf71c989c5fe50
Maurice Haro <maurice.nzai@gmail.com>
2022-07-12 04:55:55 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -19,13 +19,9 @@ void print_array(int *a, int n)
 int x;
 
 for (x = 0; x < n; x++)
-{
-printf("%d", a[x]);
-
-if (x < n - 1)
-printf(", ");
+/* if (x < n - 1) */
+printf("%d, ", a[x]);
 
 printf("\n");
 
-}
 }
