6fc9a00d56f0bf0dd4786a71c96e754fce37f457
Maurice Haro <maurice.nzai@gmail.com>
2022-07-12 00:28:51 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
  * reset_to_98 - This function takes a pointer to an integer as a parameter
  * and resets the value it points to, to 98
  *
- * @*n: Pointer to an integer value
+ * @n: Pointer to an integer value
  *
  * Return: Nothing
  */
