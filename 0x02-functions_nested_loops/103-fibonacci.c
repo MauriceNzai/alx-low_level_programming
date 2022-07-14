437acd838c63815a81ca62d396e0061e117c6998
Maurice Haro <maurice.nzai@gmail.com>
2022-07-14 05:38:33 -0700

    Task 13

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point and main block
+ *
+ * Description: Prints resuts of adding even -valued fibonacci numbers
+ *
+ * Return: Always (0) (Success)
+ */
+
+int main(void)
+{
+long int n1 = 1;
+long int n2 = 2;
+long int fn;
+long int afn;
+
+fn = afn = 0;
+
+while (fn <= 4000000)
+{
+fn = n1 + n2;
+n1 = n2;
+n2 = fn;
+
+if ((n1 % 2) == 0)
+{
+afn +=n1;
+}
+}
+
+printf("%ld\n", afn);
+
+return (0);
+
+}
+
+  
+
