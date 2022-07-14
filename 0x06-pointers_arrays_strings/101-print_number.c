fe01e444d59a09525da88ba51369e2eb49ecf537
Maurice Haro <maurice.nzai@gmail.com>
2022-07-14 03:11:02 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer using _putchar function
+ * Not allowed to use long or arrays or pointers or hard-code spcial values
+ *
+ * @n: An integer input
+ *
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+}
