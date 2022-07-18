645092d43981562462d8a0b63886bcdff8c1711d
Maurice Haro <maurice.nzai@gmail.com>
2022-07-18 07:03:16 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes
+ *
+ * Description: Locates first occarance in string s of any o f the bytes
+ *
+ * @s: The string to search in
+ * @accept: The set of bytes to search
+ *
+ * Return: Pointer to matching bytes in accept of NULL if no matching set
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+char *begin = accept;
+
+while (*s)
+{
+
+while (*accept)
+if (*accept == *s)
+return (s);
+
+accept++
+}
+
+accept = begin;
+s++
+}
+
+return (NULL);
+}
