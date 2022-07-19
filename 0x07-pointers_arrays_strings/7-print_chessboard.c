dc1bc0afc4c4eaecac9a97fd534507a8f0643787
Maurice Haro <maurice.nzai@gmail.com>
2022-07-19 04:40:33 -0700

    Task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,7 @@
 /**
  * print_chessboard - Print the chessboard
  *
- * @a: An array input, Rows and Columns of board
+ * @a: An pointer to array input, Rows and Columns of board
  *
  * Return: Nothing
  */
@@ -13,11 +13,15 @@ void print_chessboard(char (*a)[8])
 int i;
 int j;
 
-for (i = 0; i < 8; i++)
+for (i = 0; i < 64; i++)
 {
-for (j = 0; j < 8; J++)
-_putcahr(a[i][j]);
-
+if (i % 8 == 0; && i != 0)
+{
+j = 1;
 _putchar('\n');
 }
+_putchar(a[i / 8][i - j]);
+}
+
+_putchar('\n');
 }
